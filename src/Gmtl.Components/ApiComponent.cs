4e5bf96b499af72a8e54e98aaa44da9941c43174
Pawel Klimczyk <pawel@klimczyk.pl>
2022-12-23 08:43:24 +0100

    Added base ApiComponent

---
+++ src/Gmtl.Components/ApiComponent.cs
@@ -0,0 +1,24 @@
+﻿using Gmtl.HandyLib.Operations;
+using System;
+
+namespace Gmtl.Components
+{
+    public interface IApiComponent
+    {
+        OperationResult CheckConfiguration();
+        OperationResult HealthCheck();
+    }
+
+    public abstract class ApiComponent : IApiComponent
+    {
+        public OperationResult CheckConfiguration()
+        {
+            throw new NotImplementedException("Implement in inherited class!");
+        }
+
+        public OperationResult HealthCheck()
+        {
+            throw new NotImplementedException("Implement in inherited class!");
+        }
+    }
+}
