416e912e956a22de6ca2056952478c284a82baba
Pawel Klimczyk <pawel@klimczyk.pl>
2023-02-24 09:36:40 +0100

    Added Gmtl.Components.Web

---
+++ src/Script-Release.ps1
@@ -0,0 +1,75 @@
+param (
+	[switch]$patch,
+	[switch]$minor,
+	[switch]$major
+)
+
+Write-Host "This scripts increment csproj file version"
+
+$ErrorActionPreference = "Stop"
+
+$mainProjFile = Resolve-Path ".\Gmtl.Serilog\Gmtl.Serilog.csproj"
+$xml = [Xml] (Get-Content $mainProjFile)
+
+#$currentVersion = [Version]($xml.Project.PropertyGroup | ? Condition -Like '' | select -ExpandProperty Version)
+
+$currentVersion = New-Object System.Version(([System.String]$xml.Project.PropertyGroup.Version).Split('.'))
+
+# if(git status --porcelain |Where {$_ -match '^\?\?'}){
+	# throw 'Untracked files exist. Make sure your repository is clean before releasing.'
+# }
+# elseif(git status --porcelain |Where {$_ -notmatch '^\?\?'}) {
+	# throw 'Uncommitted changes exist. Make sure to commit before releasing.'
+# }
+
+Write-Host "Previous version: $($currentVersion)"
+
+if ($major) {
+    $newVersion = New-Object System.Version(($currentVersion.Major + 1), 0, 0, 0)
+} elseif ($minor) {
+    $newVersion = New-Object System.Version($currentVersion.Major, ($currentVersion.Minor + 1), 0, 0)
+} else {
+    $newVersion = New-Object System.Version($currentVersion.Major, $currentVersion.Minor, ($currentVersion.Build + 1), 0) # *patch
+}
+
+$confirmation = Read-Host "Do you want to add release tag (v$($newVersion)) and push to remote server? [y/N]"
+if ($confirmation -ne 'y') { 
+    Write-Host "Exiting..."
+    exit
+}
+
+Write-Host "New version: $($newVersion)"
+
+Get-ChildItem -Path .\ -Filter *.csproj -Recurse -File -Name| ForEach-Object {
+    try
+    {
+        #Write-Host "Checking file ${projFile}"
+        $projFile = Resolve-Path $_
+        $xml = [Xml] (Get-Content $projFile)
+        $nodeFound = $FALSE
+        $xml.Project.PropertyGroup.GetElementsByTagName("Version") | ForEach-Object {
+            $_."#text" = "$($newVersion)"
+            $nodeFound = $TRUE
+        }
+
+        if($nodeFound) {
+            $xml.Save($projFile)
+            git add $_
+            Write-Host "Updating ${projFile}" -ForegroundColor Yellow
+        }
+    }
+    catch {
+        Write-Host "Error in file ${projFile}" -ForegroundColor Red
+    }
+}
+
+
+dotnet pack .\Gmtl.Serilog\Gmtl.Serilog.csproj -c Release -o .\publish
+if ($LASTEXITCODE -ne 0) { throw "Exit code is $LASTEXITCODE" }
+dotnet nuget push .\publish\Gmtl.Serilog.$($newVersion.Major).$($newVersion.Minor).$($newVersion.Build).nupkg -k 1234 -s http://192.168.180.10:10101/nuget/packages
+if ($LASTEXITCODE -ne 0) { throw "Exit code is $LASTEXITCODE" }
+
+git commit -am "Release v$($newVersion)"
+git tag v$($newVersion)
+git push origin main --tags
+git push
