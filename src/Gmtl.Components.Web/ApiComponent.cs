c23326fba5f3819a68404e75202a5c5e98ece222
Pawel Klimczyk <pawel@klimczyk.pl>
2023-03-02 09:53:24 +0100

    Release v1.0.7.0

--- src/Gmtl.Components.Web/ApiComponent.cs
+++ src/Gmtl.Components.Web/ApiComponent.cs
@@ -3,13 +3,6 @@ using System;
 
 namespace Gmtl.Components.Web
 {
-    public interface IApiComponent
-    {
-        string Info { get; }
-        OperationResult CheckConfiguration();
-        OperationResult HealthCheck();
-    }
-
     public abstract class ApiComponent : IApiComponent
     {
         public virtual string Info => throw new NotImplementedException("Implement in inherited class!");
