afbf6adb32562271f7b9d5be04c922d01ff96da2
Pawel Klimczyk <pawel@klimczyk.pl>
2023-03-02 09:42:54 +0100

    Release v1.0.5.0

--- src/Gmtl.Components.Web/ApiComponent.cs
+++ src/Gmtl.Components.Web/ApiComponent.cs
@@ -1,16 +1,19 @@
 ﻿using Gmtl.HandyLib.Operations;
 using System;
 
-namespace Gmtl.Components
+namespace Gmtl.Components.Web
 {
     public interface IApiComponent
     {
+        string Info { get; }
         OperationResult CheckConfiguration();
         OperationResult HealthCheck();
     }
 
     public abstract class ApiComponent : IApiComponent
     {
+        public string Info => throw new NotImplementedException("Implement in inherited class!");
+
         public OperationResult CheckConfiguration()
         {
             throw new NotImplementedException("Implement in inherited class!");
