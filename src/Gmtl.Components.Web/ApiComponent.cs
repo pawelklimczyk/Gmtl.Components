7c2999a72fb91a35f192d334d681403844846af0
Pawel Klimczyk <pawel@klimczyk.pl>
2023-03-04 14:39:46 +0100

    Providing default ComponentName

--- src/Gmtl.Components.Web/ApiComponent.cs
+++ src/Gmtl.Components.Web/ApiComponent.cs
@@ -5,15 +5,14 @@ namespace Gmtl.Components.Web
 {
     public abstract class ApiComponent : IApiComponent
     {
+        public virtual string ComponentName => $"Component {GetType().FullName}";
 
-        public virtual string ComponentName => throw new NotImplementedException();
-
-        public virtual OperationResult CheckConfiguration()
+        public ComponentStatusInfo GetComponentStatusInfo()
         {
             throw new NotImplementedException("Implement in inherited class!");
         }
 
-        public ComponentStatusInfo GetComponentStatusInfo()
+        public virtual OperationResult CheckConfiguration()
         {
             throw new NotImplementedException("Implement in inherited class!");
         }
