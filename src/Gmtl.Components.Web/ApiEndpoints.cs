ea0b9e975c07c5d2cda7028d01ba3ff0faddece3
Pawel Klimczyk <pawel@klimczyk.pl>
2024-01-23 13:40:52 +0100

    Added health-check api endpoint name

--- src/Gmtl.Components.Web/ApiEndpoints.cs
+++ src/Gmtl.Components.Web/ApiEndpoints.cs
@@ -3,5 +3,6 @@
     public struct ApiEndpoints
     {
         public const string ApiInformationEndpoint = "/information-000";
+        public const string ApiHeathCheckEndpoint = "/health-check-000";
     }
 }
