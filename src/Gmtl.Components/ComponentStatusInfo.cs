2fed4a78a9e240e14dd36577829b66b0ac220cfe
Pawel Klimczyk <pawel@klimczyk.pl>
2024-01-23 13:41:25 +0100

    Improved when ComponentInfo and ChildComponentsInfo are generated

--- src/Gmtl.Components/ComponentStatusInfo.cs
+++ src/Gmtl.Components/ComponentStatusInfo.cs
@@ -9,8 +9,28 @@ namespace Gmtl.Components
         private Dictionary<string, object> _componentInfo = new Dictionary<string, object>();
         private Dictionary<string, ComponentStatusInfo> _childComponents = new Dictionary<string, ComponentStatusInfo>();
         public ComponentStatus Status { get; set; }
-        public ReadOnlyDictionary<string, object> ComponentInfo { get => new ReadOnlyDictionary<string, object>(_componentInfo); }
-        public ReadOnlyDictionary<string, ComponentStatusInfo> ChildComponentsInfo { get => new ReadOnlyDictionary<string, ComponentStatusInfo>(_childComponents); }
+
+        public ReadOnlyDictionary<string, object> ComponentInfo
+        {
+            get
+            {
+                if (_componentInfo != null && _componentInfo.Count > 0)
+                    return new ReadOnlyDictionary<string, object>(_componentInfo);
+
+                return null;
+            }
+        }
+
+        public ReadOnlyDictionary<string, ComponentStatusInfo> ChildComponentsInfo
+        {
+            get
+            {
+                if (_childComponents != null && _childComponents.Count > 0)
+                    return new ReadOnlyDictionary<string, ComponentStatusInfo>(_childComponents);
+
+                return null;
+            }
+        }
         public DateTime LastUpdate { get; private set; }
 
         public ComponentStatusInfo AddInfo(string key, object value)
